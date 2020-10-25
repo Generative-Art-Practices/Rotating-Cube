f40b732c44fe9fb72ed1929f5d7c3d4d38422fde
Abhinay Thakur <a8h1nay@gmail.com>
2020-10-25 21:01:32 +0530

    Renders the cube in the scene

--- js/app.js
+++ js/app.js
@@ -5,3 +5,18 @@ let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Creating basic cube
+let geometry = new THREE.BoxGeometry();
+let material = new THREE.MeshBasicMaterial({color: 0x0000ff});
+let cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+camera.position.z = 5;
+
+// Animation loop rendering the scene with camera
+let animate = () =>{
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
