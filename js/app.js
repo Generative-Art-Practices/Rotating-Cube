15cc71352b16542de5f00b0364348060825b24cd
Abhinay Thakur <a8h1nay@gmail.com>
2020-10-25 22:34:40 +0530

    Makes 3D scene more presentable
    Style the HTML content and described the 3D scene

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 const randomMin = 0.01;
 const randomMax = 0.05;
 let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
+let camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
 let renderer = new THREE.WebGLRenderer();
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize(480, 480);
+document.getElementById('scene').appendChild(renderer.domElement);
 
 // Creating basic cube
 let geometry = new THREE.BoxGeometry();
@@ -13,7 +13,7 @@ let material = new THREE.MeshBasicMaterial({color: 0x0000ff});
 let cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
-camera.position.z = 5;
+camera.position.z = 3;
 
 // Animation loop rendering the scene with camera
 let animate = () => {
