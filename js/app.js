1b06ef0096a778af1e6d7b3262cda3cfa820053f
Abhinay Thakur <a8h1nay@gmail.com>
2020-10-26 12:39:20 +0530

    Change the material of cube
    Change from basic material to phong  material. Added light in the scene. Set anti aliasing to be true.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
 const randomMin = 0.01;
-const randomMax = 0.05;
+const randomMax = 0.03;
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-let renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer({antialias: true});
 
 renderer.setSize(480, 480);
 document.getElementById('scene').appendChild(renderer.domElement);
 
 // Creating basic cube
 let geometry = new THREE.BoxGeometry();
-let material = new THREE.MeshBasicMaterial({color: 0x0000ff});
+let material = new THREE.MeshPhongMaterial({
+    color: 0x0000ff,
+    flatShading: true,
+    ambient: 0x050505, 
+    color: 0x0033ff, 
+    specular: 0x555555, 
+    shininess: 30
+  });
 let cube = new THREE.Mesh(geometry, material);
+let light = new THREE.DirectionalLight( 0xffffff );
 
 scene.add(cube);
+scene.add(light);
+
+light.position.set( 0, 1, 1 ).normalize();
+material.needsUpdate = true
 camera.position.z = 3;
 
 // Animation loop rendering the scene with camera
